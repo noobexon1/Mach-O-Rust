9f30c8189805e96a50468eb1614131e8548656a4
ligma <ligma@sugma.com>
2024-04-22 16:08:39 +0300

    refactored lib.rs intoo main.rs because this is an executable

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use std::fs::File;
+use std::path::PathBuf;
+
 use clap::Parser;
 
-use mach_o_rust::{Args, run};
+use crate::parser::parse;
+
+mod parser;
+mod header;
+
+/// A command-line tool written in rust to view and explore files with mach-o format.
+#[derive(Parser)]
+#[command(name = "Mach_O_Rust")]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// Input mach-o file
+    #[arg(short, long, value_name = "FILE_PATH", required = true)]
+    pub input: PathBuf,
+}
 
 fn main() {
     let args = Args::parse();
-    run(&args);
+    match File::open(&args.input.as_path()) {
+        Ok(mut file) => parse(&mut file),
+        Err(e) => eprintln!("Failed to open input file: {}", e),
+    }
 }
