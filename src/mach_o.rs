5c55a4b3e5f5e94e2eb48ddaac9633305acfc565
noobexon <noamf92@gmail.com>
2024-06-12 19:05:43 +0300

    started working on symtab

--- src/mach_o.rs
+++ src/mach_o.rs
@@ -1,6 +1,8 @@
 use crate::header::MachHeader;
 use crate::load_commands::{LcStr, LoadCommand, Section};
 
+//TODO: use Option<T> to init mach-o struct at the beginning with empty values and then pass it to the parser instead of the header and load_commands at the beginning...
+
 pub struct MachO {
     pub header: MachHeader,
     pub load_commands: (Vec<LoadCommand>, Vec<Vec<Section>>, Vec<LcStr>),
